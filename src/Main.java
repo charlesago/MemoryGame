28001562eadb782295b8dc98788f8c0632dc00a4
charles <chacha.agostinelli@gmail.Com>
2024-10-29 16:58:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+
+
+    }
+}
